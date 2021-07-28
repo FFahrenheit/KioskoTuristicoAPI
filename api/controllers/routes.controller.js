ca25711af607c4a0f46e93f0ac811cfaed96162f
Iván L. Murillo <diskman199@gmail.com>
2021-07-27 21:04:40 -0500

    obtener información de línea listo

--- api/controllers/routes.controller.js
+++ api/controllers/routes.controller.js
@@ -1,14 +1,42 @@
 const Sql = require('../db/sql');
 
+exports.getLines = async(req, res)=>{
+    try{
+        const query = `SELECT id_linea,
+        (SELECT estacion FROM kiosco as k1 WHERE k1.id_linea = k0.id_linea AND k1.id_estacion = 0) as origen,
+        (SELECT estacion FROM kiosco as k2 WHERE k2.id_linea = k0.id_linea AND 
+         k2.id_estacion = (SELECT MAX(id_estacion) FROM kiosco WHERE id_linea = k0.id_linea)) as destino
+        FROM kiosco as k0
+        GROUP BY k0.id_linea
+        ORDER BY k0.id_linea`;
+        
+        const lineas = await Sql.query(query);
+
+        return res.json({
+            ok: true,
+            lineas
+        });
+    }catch(e){
+        console.log(e);
+        return res.status(500).send({
+            ok: false,
+            error: e
+        });
+    }
+}
+
 exports.getStops = async(req, res) =>{
     try{
         const query = `SELECT DISTINCT estacion AS 'nombre',
                 0 AS 'tipo'
                 FROM kiosco
+                WHERE estatus = 0
             UNION
         SELECT DISTINCT punto_de_interes AS 'nombre',
                 1 as 'tipo'
-                FROM puntos_de_interes`;
+                FROM puntos_de_interes, kiosco
+                WHERE puntos_de_interes.id_kiosco = kiosco.id_kiosco
+                AND kiosco.estatus = 0`;
         
         const paradas = await Sql.query(query);
 
--- api/routes/routes.routes.js
+++ api/routes/routes.routes.js
@@ -2,5 +2,8 @@ const Routes = require('../controllers/routes.controller');
 
 module.exports = (app) =>{
     app.route('/paradas')
-    .get(Routes.getStops);
+    .get(Routes.getStops)
+    
+    app.route('/lineas')
+    .get(Routes.getLines);
 }
